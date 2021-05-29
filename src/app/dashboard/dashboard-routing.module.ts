7e5bf212c7fe3289cfd55592d2200cf83a6877be
Herbert Pro <herbertprocastillo@gmail.com>
2021-05-28 19:38:25 -0500

    thrid commit

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard.component';
+import {SessionGuard} from '../auth/guards/session.guard';
 
-const routes: Routes = [{ path: '', component: DashboardComponent }];
+const routes: Routes = [{path: '', component: DashboardComponent, canActivate: [SessionGuard]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {
+}
